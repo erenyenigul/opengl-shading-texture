e6cf3fc5ee79873a883d96e2c2ee4aca40721082
erenyenigul <tc.eren.yenigul@gmail.com>
2023-05-24 12:43:04 +0300

    fix: missing shader updates.

--- src/shader/fshader.glsl
+++ src/shader/fshader.glsl
@@ -1,31 +1,39 @@
-#version 150
-// per-fragment interpolated values from the vertex shader
+// Per-fragment interpolated values from the vertex shader
+
+#version 410
+
 in  vec3 fN;
 in  vec3 fL;
-in  vec3 fE;
-
-out vec4 fColor;
+in  vec3 fV;
 
 uniform vec4 AmbientProduct, DiffuseProduct, SpecularProduct;
-uniform mat4 ModelView;
-uniform vec4 LightPosition;
 uniform float Shininess;
+out vec4 fcolor;
 
-void main() {
-    // Normalize the input lighting vectors
-    vec3 N = normalize(fN);
-    vec3 E = normalize(fE);
-    vec3 L = normalize(fL);
-    vec3 H = normalize( L + E );
-    vec4 ambient = AmbientProduct;
-    float Kd = max(dot(L, N), 0.0);
-    vec4 diffuse = Kd*DiffuseProduct;
-    float Ks = pow(max(dot(N, H), 0.0), Shininess);
-    vec4 specular = Ks*SpecularProduct;
-    // discard the specular highlight if the light’s behind the vertex
-    if( dot(L, N) < 0.0 ) {
-        specular = vec4(0.0, 0.0, 0.0, 1.0);
-    }
-    fColor = ambient + diffuse + specular;
-    fColor.a = 1.0;
-}
+void main() 
+{ 
+        // Normalize the input lighting vectors
+        vec3 N = normalize(fN);
+        vec3 V = normalize(fV);
+        vec3 L = normalize(fL);
+
+        vec3 H = normalize( L + V );
+        
+        vec4 ambient = AmbientProduct;
+
+        float Kd = max(dot(L, N), 0.0);
+        vec4 diffuse = Kd*DiffuseProduct;
+        
+        float Ks = pow(max(dot(N, H), 0.0), Shininess);
+        vec4 specular = Ks*SpecularProduct;
+
+        // discard the specular highlight if the light's behind the vertex
+        if( dot(L, N) < 0.0 ) {
+            specular = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+
+    fcolor = ambient + diffuse + specular;
+        fcolor.a = 1.0;
+
+} 
+
--- src/shader/vshader.glsl
+++ src/shader/vshader.glsl
@@ -1,25 +1,33 @@
-#version 150
-in   vec4 vPosition;
-in   vec3 vNormal;
-// output values that will be interpolated per-fragment
+#version 410
+
+in vec4 vPosition;
+in vec3 vNormal;
+
+// output values that will be interpretated per-fragment
 out  vec3 fN;
-out  vec3 fE;
+out  vec3 fV;
 out  vec3 fL;
 
 uniform mat4 ModelView;
+uniform mat4 Transformation;
 uniform vec4 LightPosition;
 uniform mat4 Projection;
-uniform vec4 positionAtT;
 
-void main() {
+void main()
+{
+    // Transform vertex position into camera (eye) coordinates
+    vec3 pos = (ModelView * Transformation* vPosition).xyz;
+    
+    fN = (ModelView * vec4(vNormal, 0.0)).xyz; // normal direction in camera coordinates
 
-    fN = vNormal;
-    fE = vPosition.xyz;
-    fL = LightPosition.xyz;
+    fV = -pos; //viewer direction in camera coordinates
+
+    fL = LightPosition.xyz; // light direction
 
     if( LightPosition.w != 0.0 ) {
-        fL = LightPosition.xyz - vPosition.xyz;
+        fL = LightPosition.xyz - pos;  //point light source
     }
+
     
-    gl_Position = Projection*( vPosition + positionAtT)  ;
+    gl_Position = Projection * ModelView * Transformation* vPosition;
 }
